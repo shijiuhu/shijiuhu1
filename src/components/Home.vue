74a190e3455e841981e5a83daddf11e13d31cd39
coderyt <2605564305@qq.com>
2022-09-16 23:58:21 +0800

    增加随机数生成的几种方法

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,6 +12,12 @@
       <li><span @click="questionNaire">调查问卷</span></li>
       <li><span @click="provinceTree">省市树</span></li>
       <li><span @click="getEnvironment">获取本地开发或生产环境：</span>{{ localEnvironment }}</li>
+      <li><span @click="uuid()">生成随机数1：</span>{{ randomNumber1 }}</li>
+      <li><span @click="guid">生成随机数2：</span>{{ randomNumber2 }}</li>
+      <li><span @click="uuid2">生成随机数3：</span>{{ randomNumber3 }}</li>
+      <li><span @click="nanoid1">生成随机数4：</span>{{ randomNumber4 }}</li>
+      <li><span @click="nanoid2">生成随机数5：</span>{{ randomNumber5 }}</li>
+      <li><span @click="nanoid3">生成随机数6：</span>{{ randomNumber6 }}</li>
       <li><span @click="getLocalJson">获取本地json文件</span></li>
     </ol>
     
@@ -43,6 +49,7 @@
 
 <script>
 import { request } from "../network/index.js";
+import { nanoid, customAlphabet } from 'nanoid'
 
 export default {
   name: "Home",
@@ -79,6 +86,12 @@ export default {
           content: "强烈推荐",
         },
       ],
+      randomNumber1: '',
+      randomNumber2: '',
+      randomNumber3: '',
+      randomNumber4: '',
+      randomNumber5: '',
+      randomNumber6: ''
     };
   },
   methods: {
@@ -90,6 +103,75 @@ export default {
       this.localEnvironment = process.env.NODE_ENV;
       console.log(process.env, "--------this.localEnvironment");
     },
+
+    // 生成随机数方法1，这里写参数的话调用的dom要加上()，不加具体参数则使用这里默认的36；
+    // 这是我自己写的，两数重复的概率极小，10个数字+26个大写字母+26个小写字母，共62个字符。
+    uuid(count=36) {
+      console.log(count)
+      const s = []
+      const digits = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      for (let i = 0; i < count; i++) {
+        s[i] = digits.charAt(Math.floor(Math.random() * digits.length))
+      }
+      this.randomNumber1 = s.join('')
+      console.log(this.randomNumber1)
+    },
+
+    // 生成随机数方法2
+    guid() {
+      this.randomNumber2 = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 | 0,
+              v = c == 'x' ? r : (r & 0x3 | 0x8)
+         return v.toString(16)
+      })
+      console.log(this.randomNumber2)
+    },
+
+    // 生成随机数方法3
+    uuid2() {
+      const s = []
+      const hexDigits = '0123456789abcdef'
+      for (let i = 0; i < 36; i++) {
+        s[i] = hexDigits.charAt(Math.floor(Math.random() * 0x10))
+      }
+      s[14] = '4'
+      s[19] = hexDigits.charAt((s[19] & 0x3) | 0x8)
+      s[8] = s[13] = s[18] = s[23] = '-'
+      this.randomNumber3 = s.join('')
+      console.log(this.randomNumber3)
+    },
+
+    // 生成随机数方法4，import { nanoid } from 'nanoid' 主要可以通过npm包的形式安装，核心代码如下
+    nanoid1() {
+      const urlAlphabet = 'useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict'
+      let nanoId = (size = 21) => {
+        let id = ''
+        // A compact alternative for `for (var i = 0; i < step; i++)`.
+        let i = size
+        while (i--) {
+          // `| 0` is more compact and faster than `Math.floor()`.
+          id += urlAlphabet[(Math.random() * 64) | 0]
+        }
+        return id
+      }
+      this.randomNumber4 = nanoId()
+      console.log(this.randomNumber4)
+    },
+    
+    // 生成随机数方法5，使用npm安装的依赖，nanoid方法，不指定默认长度为21，nanoid()
+    nanoid2() {
+      this.randomNumber5 = nanoid()
+      console.log(this.randomNumber5)
+    },
+
+    // 生成随机数方法6，使用npm安装的依赖，不指定默认长度为21，customAlphabet()
+    nanoid3() {
+      // 加第二个参数就是生成的位数，不加默认长度21
+      const nanoid = customAlphabet('ABCDEF1234567890', 12)
+      this.randomNumber6 = nanoid()
+      console.log(this.randomNumber6)
+    },
+
     getLocalJson() {
       const localRequest = request({
         url: "/LocalTest.json",
