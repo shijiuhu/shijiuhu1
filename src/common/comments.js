efc2fc5bf249fd73dd6f995c479c20fa70a0bdc5
coderyt <2605564305@qq.com>
2022-09-28 19:33:49 +0800

    首页评论内容抽取成单独js

---
+++ src/common/comments.js
@@ -0,0 +1,24 @@
+const comments = [
+  {
+      imgUrl: require("@/assets/img/comments/npsFeel1.png"),
+      content: "强烈不推荐",
+  },
+  {
+      imgUrl: require("@/assets/img/comments/npsFeel2.png"),
+      content: "不推荐",
+  },
+  {
+      imgUrl: require("@/assets/img/comments/npsFeel3.png"),
+      content: "一般般",
+  },
+  {
+      imgUrl: require("@/assets/img/comments/npsFeel4.png"),
+      content: "推荐",
+  },
+  {
+      imgUrl: require("@/assets/img/comments/npsFeel5.png"),
+      content: "强烈推荐",
+  },
+]
+
+export default comments
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -66,6 +66,7 @@
 
 <script>
 import { request } from "../network/index.js"
+import comments from "@/common/comments";
 
 export default {
   name: "Home",
@@ -78,29 +79,7 @@ export default {
       leetCodePractice: "https://leetcode.cn/problems/palindrome-number/",
       localTestJson: [],
       localEnvironment: "",
-      comments: [
-          {
-              imgUrl: require("@/assets/img/comments/npsFeel1.png"),
-              content: "强烈不推荐",
-          },
-          {
-              imgUrl: require("@/assets/img/comments/npsFeel2.png"),
-              content: "不推荐",
-          },
-          {
-              imgUrl: require("@/assets/img/comments/npsFeel3.png"),
-              content: "一般般",
-          },
-          {
-              imgUrl: require("@/assets/img/comments/npsFeel4.png"),
-              content: "推荐",
-          },
-          {
-              imgUrl: require("@/assets/img/comments/npsFeel5.png"),
-              content: "强烈推荐",
-          },
-      ],
-
+      comments,
     };
   },
   methods: {
